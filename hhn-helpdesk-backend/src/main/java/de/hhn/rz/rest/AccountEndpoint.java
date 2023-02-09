f8fe664339a9e76fe0a1488d52aaa058c099aa1f
Richard Zowalla <richard.zowalla@hs-heilbronn.de>
2023-02-09 10:35:42 +0100

    Revert "see what will happen with pdf download"
    This reverts commit 3a864f4ac65211a23fecf66227ceabddd1a50e12.

--- hhn-helpdesk-backend/src/main/java/de/hhn/rz/rest/AccountEndpoint.java
+++ hhn-helpdesk-backend/src/main/java/de/hhn/rz/rest/AccountEndpoint.java
@@ -11,8 +11,6 @@ import de.hhn.rz.services.AccountCredentialService;
 import de.hhn.rz.services.AuditLogService;
 import de.hhn.rz.services.KeycloakService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.lang.Nullable;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,11 +20,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
 
 import java.util.List;
-
-import static javax.ws.rs.core.HttpHeaders.CONTENT_DISPOSITION;
 
 @RestController
 @CrossOrigin(origins = {"http://localhost:8080", "http://localhost:3000"})
@@ -78,20 +73,11 @@ public class AccountEndpoint extends AbstractService {
     }
 
     @PostMapping("create")
-    public ResponseEntity<StreamingResponseBody> create(@RequestBody AccountCreate accountCreate) {
+    public @ResponseBody byte[] create(@RequestBody AccountCreate accountCreate) {
         checkParameter(accountCreate);
         checkParameter(accountCreate.location());
         final int amount = (accountCreate.amount() == null || accountCreate.amount() <= 0) ? 10 : accountCreate.amount();
         auditLogService.audit(AuditAction.CREATE, "amount=" + accountCreate.amount(), "location=" + accountCreate.location());
-        final byte[] pdf = accountCredentialService.createCredentials(new AccountCreate(accountCreate.location(), amount));
-
-        StreamingResponseBody responseBody = response -> {
-            response.write(pdf);
-        };
-
-        return ResponseEntity.ok()
-                .header(CONTENT_DISPOSITION, "attachment; filename=\"credentials.pdf\"")
-                .contentType(MediaType.APPLICATION_PDF).body(responseBody);
-
+        return accountCredentialService.createCredentials(new AccountCreate(accountCreate.location(), amount));
     }
 }
