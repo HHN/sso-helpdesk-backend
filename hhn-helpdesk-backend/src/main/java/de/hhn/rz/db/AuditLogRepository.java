37aedc62f4638ffebdf2a64dea89ee2b62dc1259
Richard Zowalla <richard.zowalla@hs-heilbronn.de>
2023-02-17 09:43:12 +0100

    Remove unused imports

--- hhn-helpdesk-backend/src/main/java/de/hhn/rz/db/AuditLogRepository.java
+++ hhn-helpdesk-backend/src/main/java/de/hhn/rz/db/AuditLogRepository.java
@@ -4,8 +4,6 @@ import de.hhn.rz.db.entities.AuditLogEntry;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface AuditLogRepository extends PagingAndSortingRepository<AuditLogEntry, Long> {
 
